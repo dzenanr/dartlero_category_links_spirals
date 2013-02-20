fbb7541048552b60827d5f3ef4e8def0e387efaa
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-02-20 16:22:07 -0500

    s07: remove link; add link tests

---
+++ dartlero_category_links_s07/web/component/link/link_edit.dart
@@ -0,0 +1,31 @@
+import 'dart:html';
+import 'dart:uri';
+
+import 'package:dartlero_category_links/dartlero_category_links.dart';
+import 'package:web_ui/web_ui.dart';
+
+class LinkEdit extends WebComponent {
+  Category category;
+  Links links;
+  Link link;
+
+  update() {
+    InputElement webLink = query("#edit-link-url");
+    InputElement description = query("#edit-link-description");
+    Element message = query("#edit-link-message");
+    var error = false;
+    message.text = '';
+    if (webLink.value.trim() == '') {
+      message.text = 'web link is mandatory; ${message.text}';
+      error = true;
+    }
+    if (!error) {
+      link.url = new Uri.fromString(webLink.value);
+      link.description = description.value;
+      var linkTable = document.query('x-link-table').xtag;
+      linkTable.showLinkEdit = false;
+    }
+  }
+
+}
+
